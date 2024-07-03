2af70e48b547946340a614234f7e11c6ecfc29cd
Yann-Thomas Le Moigne <yann.tho.le.moigne@gmail.com>
2024-07-03 08:48:07 +0200

    Ajout des ressources à télécharger

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,47 @@
 <script lang="ts">
-  import Dice from './lib/Dice.svelte';
-import Question from './lib/Question.svelte'
-  import { initQuestions, reloadGame, getCategories } from './lib/question'
+  import Dice from "./lib/Dice.svelte";
+  import Question from "./lib/Question.svelte";
+  import { initQuestions, reloadGame, getCategories } from "./lib/question";
   // Initialise les questions
-  initQuestions()
+  initQuestions();
   // Initialise les catégories
-  const categories = getCategories()
+  const categories = getCategories();
 
   // Selection d'une catégorie de question
   let categorySelected: string | null = null;
+
+  function print(url: string): void {
+    let fenetreImpression = window.open(url);
+    if (fenetreImpression) {
+      fenetreImpression.onload = function () {
+        fenetreImpression.print();
+        // Ferme l'onglet après un délai pour donner le temps à l'impression de se lancer
+        setTimeout(() => {
+          fenetreImpression.close();
+        }, 1000);
+      };
+    }
+  }
 </script>
 
-<main> 
+<main>
   {#if !!categorySelected}
-  <Question category={categorySelected} on:newQuestion={() => categorySelected = null}/>
-    {:else}
+    <Question
+      category={categorySelected}
+      on:newQuestion={() => (categorySelected = null)}
+    />
+  {:else}
+    <div class="ressources">
+      <button on:click={() => print("./plateau.png")}
+        >Télécharger le plateau</button
+      >
+      <button on:click={() => print("./pions.jpg")}
+        >Télécharger les pions</button
+      >
+      <button
+        ><a href="./regle.jpg" target="_blank">Consulter les règles</a></button
+      >
+    </div>
     <div class="setting">
       <button on:click={() => reloadGame()}>Recharger le jeu</button>
     </div>
@@ -25,7 +52,10 @@ import Question from './lib/Question.svelte'
     <ul>
       {#each categories as category}
         <li>
-          <button class={category} on:click={() => categorySelected = category}>{category}</button>
+          <button
+            class={category}
+            on:click={() => (categorySelected = category)}>{category}</button
+          >
         </li>
       {/each}
     </ul>
@@ -43,9 +73,22 @@ import Question from './lib/Question.svelte'
     }
   }
 
-  div.setting {
+  div.ressources {
     position: absolute;
     top: 0;
     left: 0;
+    display: flex;
+    flex-direction: column;
+
+    & button > a {
+      color: black;
+      text-decoration: none;
+    }
+  }
+
+  div.setting {
+    position: absolute;
+    top: 0;
+    right: 0;
   }
 </style>
