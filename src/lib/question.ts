03b26b765fb153fa042880b0e9400f79bb316f4f
Yann-Thomas LE MOIGNE <yann.tho.le.moigne@gmail.com>
2024-08-27 17:42:19 +0200

    feature/file - Utilisation d'un fichier différent dépendant du nom de l'url (#7)

--- src/lib/question.ts
+++ src/lib/question.ts
@@ -2,6 +2,18 @@ import questionsFromFile from "../assets/questions.json";
 
 const QUESTIONS_LOCAL_STORAGE_KEY = "questions_trivial_pursuit";
 
+const getSlugFromUrl = (): string => {
+  return window.location.pathname.split("/").pop() || "";
+};
+
+const getQuestionsLocalStorageKey = (): string => {
+  const slug = getSlugFromUrl();
+  if (slug === "") {
+    return QUESTIONS_LOCAL_STORAGE_KEY;
+  }
+  return QUESTIONS_LOCAL_STORAGE_KEY + "_" + slug;
+};
+
 export type Question = {
   question: string;
   answer: string;
@@ -23,11 +35,29 @@ Array.prototype.shuffle = function () {
   return this;
 };
 
-export const initQuestions = (): void => {
-  const storedQuestions = localStorage.getItem(QUESTIONS_LOCAL_STORAGE_KEY);
+const getQuestions = async (): Promise<Question[]> => {
+  try {
+    const slug = getSlugFromUrl();
+    if (slug === "") {
+      return questionsFromFile;
+    }
+    const questionsModule = await import(`../assets/questions-${slug}.json`);
+    return questionsModule.default;
+  } catch (e) {
+    console.error(e);
+    alert(
+      "Une erreur est survenue lors de la récupération des questions. Veuillez recharger la page."
+    );
+    return [];
+  }
+};
+
+export const initQuestions = async (): Promise<void> => {
+  const storedQuestions = localStorage.getItem(getQuestionsLocalStorageKey());
+  const questions = await getQuestions();
   if (
     storedQuestions !== null &&
-    JSON.parse(storedQuestions).length !== questionsFromFile.length &&
+    JSON.parse(storedQuestions).length !== questions.length &&
     !confirm(
       "Les questions ont déjà été initialisées. Voulez-vous les recharger ?"
     )
@@ -35,32 +65,35 @@ export const initQuestions = (): void => {
     return; // Les questions ont déjà été initialisées
   }
   localStorage.setItem(
-    QUESTIONS_LOCAL_STORAGE_KEY,
-    JSON.stringify(questionsFromFile.shuffle())
+    getQuestionsLocalStorageKey(),
+    JSON.stringify(questions.shuffle())
   );
 };
 
 export const reloadQuestionsByCategory = (category: string): void => {
   let questions = JSON.parse(
-    localStorage.getItem(QUESTIONS_LOCAL_STORAGE_KEY) || "[]"
+    localStorage.getItem(getQuestionsLocalStorageKey()) || "[]"
   ) as Question[];
   const questionsByCategory = questionsFromFile
     .filter((q) => q.category === category)
     .shuffle();
   questions = questions.concat(questionsByCategory);
-  localStorage.setItem(QUESTIONS_LOCAL_STORAGE_KEY, JSON.stringify(questions));
+  localStorage.setItem(
+    getQuestionsLocalStorageKey(),
+    JSON.stringify(questions)
+  );
   alert("Les questions de la catégorie " + category + " ont été rechargées");
 };
 
-export const reloadGame = (): void => {
-  localStorage.removeItem(QUESTIONS_LOCAL_STORAGE_KEY);
-  initQuestions();
+export const reloadGame = async (): Promise<void> => {
+  localStorage.removeItem(getQuestionsLocalStorageKey());
+  await initQuestions();
   alert("Le jeu a été réinitialisé");
 };
 
 export const getQuestionByCategory = (category: string): Question | null => {
   let questions = JSON.parse(
-    localStorage.getItem(QUESTIONS_LOCAL_STORAGE_KEY) || "[]"
+    localStorage.getItem(getQuestionsLocalStorageKey()) || "[]"
   ) as Question[];
   const index = questions.findIndex((q) => q.category === category);
 
@@ -70,13 +103,16 @@ export const getQuestionByCategory = (category: string): Question | null => {
 
   const question = questions[index];
   questions.splice(index, 1);
-  localStorage.setItem(QUESTIONS_LOCAL_STORAGE_KEY, JSON.stringify(questions));
+  localStorage.setItem(
+    getQuestionsLocalStorageKey(),
+    JSON.stringify(questions)
+  );
 
   return question;
 };
 
-export const getCategories = (): string[] => {
-  return questionsFromFile
+export const getCategories = async (): Promise<string[]> => {
+  return (await getQuestions())
     .map((q) => q.category)
     .filter((value, index, self) => self.indexOf(value) === index)
     .sort();
